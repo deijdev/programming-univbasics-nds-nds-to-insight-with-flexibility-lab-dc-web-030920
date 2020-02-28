a2a35c694ac6360ee014b738cb35ba881f7e872a
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 05:09:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -92,13 +92,13 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
     new_arr = []
-  source.each do |movies_info|
+ while source.each do |movies_info|
     movies_info_with_director = movies_with_director_key(movies_info[:name], movies_info[:movies])
     new_arr << movies_info_with_director
-  end #each
+  end 
   new_arr
 end
-end
+
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
